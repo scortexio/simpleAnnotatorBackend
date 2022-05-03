5d34054cde7f783dc862eaf08dc4bdf1597cd24d
Noé Rubinstein <nrubinstein@scortex.io>
2022-05-03 18:52:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import Response
+
+app = FastAPI()
+
+
+class Annotation(BaseModel):
+    x: int
+    y: int
+    label: str
+
+
+class Image(BaseModel):
+    url: str
+    annotations: List[Annotation] = []
+
+database = [
+    Image(url=f"https://picsum.photos/id/{i}/800/600")
+    for i in range(20)
+]
+
+@app.get("/images")
+async def images():
+    return [{"id": f"{i}", "url": image.url} for i, image in enumerate(database)]
+
+
+@app.get("/annotation/{id}")
+async def annotations(id: str):
+    image = database[int(id)]
+    return image.annotations
+
+@app.put("/annotation/{id}")
+async def annotations(id: str, body: List[Annotation]):
+    image = database[int(id)]
+    image.annotations = body
+    return Response()
